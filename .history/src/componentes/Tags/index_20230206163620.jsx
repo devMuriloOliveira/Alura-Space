60fd00fd00c959a5d09f9b0b2bc294d3383c9e38
murilo <muriloleiteoliveira1@gmail.com>
2023-04-06 14:49:25 -0300

    Alura-Space

---
+++ .history/src/componentes/Tags/index_20230206163620.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Tags.module.scss'
+
+export default function Tags({ tags }) {
+  return (
+    <div className={styles.tags}>
+      <p>Filtre por tags:</p>
+      <ul className={styles.tags__lista}>
+        {tags.map(tag => {
+          <li>{tag}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
---
+++ .history/src/componentes/Tags/index_20230206163821.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './Tags.module.scss'
+
+export default function Tags({ tags }) {
+  return (
+    <div className={styles.tags}>
+      <p>Filtre por tags:</p>
+      <ul className={styles.tags__lista}>
+        {tags.map(tag => {
+          return (
+            <li key={tag}>{tag}</li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
